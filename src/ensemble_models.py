13186ca9380039ed07666031e78d98bdbaf4de15
Varun Lakshmanan <varunlakshmanan@outlook.com>
2020-05-27 22:52:20 -0400

    Fixed ValueError
    Wrong data type was being passed into the VotingClassifier/VotingRegressor fit method

--- src/ensemble_models.py
+++ src/ensemble_models.py
@@ -1,5 +1,5 @@
 from itertools import combinations
-from varname import nameof
+from itertools import chain
 from sklearn.ensemble import VotingClassifier
 from sklearn.ensemble import VotingRegressor
 from sklearn.metrics import roc_auc_score
@@ -8,9 +8,10 @@ best_voting_estimator = []
 
 '''
 Problems: 
-- names[] array isn't being populated correctly 
-- all_estimator_combinations has length 1
-- estimator_combination has length 1
+- change accuracy metric 
+- ConvergenceWarning: Liblinear failed to converge, increase the number of iterations.
+- Change suggest_int parameters in optimize_hyperparams file 
+- Fix other parameters
 '''
 
 
@@ -19,9 +20,9 @@ def ensemble_models(optimized_estimators, x_train, y_train, x_test, y_test, is_c
     all_estimator_combinations = []
 
     # Store combinations of length 2 to length MAX_ENSEMBLES of all estimators in a list
-    for i in reversed(range(2, MAX_ENSEMBLES)):
-        temp_estimator_combinations = list(combinations(optimized_estimators, i))
-        all_estimator_combinations.append(temp_estimator_combinations)
+    for i in reversed(range(2, MAX_ENSEMBLES + 1)):
+        temp_estimator_combinations = combinations(optimized_estimators, i)
+        all_estimator_combinations.extend(temp_estimator_combinations)
 
     voting_estimators = []
     global best_voting_estimator
@@ -34,7 +35,8 @@ def ensemble_models(optimized_estimators, x_train, y_train, x_test, y_test, is_c
             for estimator in estimator_combination:
                 names.append(str(estimator))
             estimator_dict = dict(zip(names, estimator_combination))
-            voting_estimators.append(VotingClassifier(estimator_dict))
+            estimator_list = list(estimator_dict.items())
+            voting_estimators.append(VotingClassifier(estimator_list))
 
         max_auc = -1
 
@@ -47,7 +49,7 @@ def ensemble_models(optimized_estimators, x_train, y_train, x_test, y_test, is_c
             if auc > max_auc:
                 best_voting_estimator = voting_estimator
 
-        print("Highest AUC: " + max_auc)
+        print("Highest AUC: " + str(max_auc))
         return best_voting_estimator
 
     else:
@@ -58,9 +60,14 @@ def ensemble_models(optimized_estimators, x_train, y_train, x_test, y_test, is_c
             for estimator in estimator_combination:
                 names.append(str(estimator))
             estimator_dict = dict(zip(names, estimator_combination))
-            voting_estimators.append(VotingRegressor(estimator_dict))
+            estimator_list = list(estimator_dict.items())
+            voting_estimators.append(VotingRegressor(estimator_list))
 
         max_auc = -1
+
+        #for i in voting_estimators:
+        #    print(type(i).__name__)
+        #print("Done!")
 
         # Find the voting regressor with the highest AUC
         for voting_estimator in voting_estimators:
@@ -71,5 +78,5 @@ def ensemble_models(optimized_estimators, x_train, y_train, x_test, y_test, is_c
             if auc > max_auc:
                 best_voting_estimator = voting_estimator
 
-        print("Highest AUC: " + max_auc)
+        print("Highest AUC: " + str(max_auc))
         return best_voting_estimator, max_auc
