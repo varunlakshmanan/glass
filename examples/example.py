8c9ee8c4b5c546b548bba407a30968f173846cec
Varun Lakshmanan <varunlakshmanan@outlook.com>
2020-06-08 17:47:09 -0400

    Fixed the train-validation-test split

--- examples/example.py
+++ examples/example.py
@@ -3,7 +3,7 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import RobustScaler
 
-data_file_path = os.path.join(os.path.dirname(os.path.dirname(__file__)), 'examples', 'data.csv')
+data_file_path = "D:/Documents/Projects/Glass/data.csv"
 data = pd.read_csv(data_file_path)
 
 scaler = RobustScaler()
@@ -15,9 +15,9 @@ y = data.death
 scaled_X = scaler.fit_transform(X)
 
 # 70% training, 15% validation, 15% test
-X_train, X_test, y_train, y_test = train_test_split(scaled_X, y, test_size=0.3)
-X_train, X_validation, y_train, y_validation = train_test_split(X_train, y_train, test_size=(0.15/0.7))
+X_train, X_test, y_train, y_test = train_test_split(scaled_X, y, test_size=0.15)
+X_train, X_validation, y_train, y_validation = train_test_split(X_train, y_train, test_size=(0.15/0.85))
 
 ensemble = GlassRegressor()
-ensemble.fit(X_train, y_train, X_validation, y_validation, timeout=15)
+ensemble.fit(X_train, y_train, X_validation, y_validation, timeout=15, max_in_ensemble=4)
 ensemble.describe(X_test, y_test)
